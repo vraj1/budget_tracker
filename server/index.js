385d0317ce1668c6845179a544fd9bb745d89794
vraj1 <vraj.patel@mail.utoronto.ca>
2023-10-05 15:11:11 -0400

    adding server files and restructing app structure

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res)=>{
+    res.send('New APP');
+})
+
+app.listen(8001, () => {
+    console.log("Listening on port" )
+});
